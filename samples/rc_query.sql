44f749177ef3d3a0dd96fdf784785f2f84d4ee0e
Silvio Silva | Stone <silvio.silva@stone.local@BRRIOLN012383.stone.local>
2019-02-25 13:04:11 -0300

    Added rc_query.sql

---
+++ samples/rc_query.sql
@@ -0,0 +1,28 @@
+SELECT
+     call.idcallcent_queuecalls as demand_id,
+     (
+       CASE
+         WHEN (call.call_result IN ('WPS', 'WPCLBS', 'WCPLBS', 'WCPL', 'WPFS', 'WPCL')) OR (call.call_result IN ('W', 'WP') AND call.to_dn != '')
+           THEN FALSE
+         ELSE TRUE
+       END
+     ) AS missed,
+     EXTRACT(seconds from (call.ts_polling + call.ts_waiting)::time)::numeric::integer + EXTRACT(minutes from (call.ts_polling + call.ts_waiting)::time)::numeric::integer * 60 + EXTRACT(hours from (call.ts_polling + call.ts_waiting)::time)::numeric::integer * 3600 as waiting_time,
+     call.from_userpart as customer,
+     call.time_start,
+     EXTRACT(seconds from call.time_end - call.time_start) + 60*EXTRACT(minutes from call.time_end - call.time_start) + 3600*EXTRACT(hours from call.time_end - call.time_start) as duration,
+     call.q_num as queue,
+     call.to_dn AS agent
+   FROM
+     callcent_queuecalls call
+   WHERE
+     --call.q_num != '8011'
+     --AND
+     call.time_end >= '${formattedStartDate}'
+     AND call.time_end <= '${formattedEndDate}'
+     AND call.call_result IN ('WPS', 'WP', 'W', 'WPN', 'WN', 'WPCLBS', 'WPCLF', 'WCPLBS', 'WCPL', 'WPFS', 'WCPLF', 'WPCL')
+     AND (
+       call.call_result IN ('WPS', 'WPCLBS', 'WCPLBS', 'WCPL', 'WPFS', 'WPCL')
+       OR (call.call_result NOT IN ('WPS', 'WPCLBS', 'WCPLBS', 'WCPL', 'WPFS', 'WPCL') AND call.to_dn = '')
+       OR (call.call_result IN ('W', 'WP') AND call.to_dn != '')
+     );
